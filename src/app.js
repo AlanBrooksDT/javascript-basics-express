35ea9ce3ea4a1f18e0bce86c4cbc9bb137d36230
Alan Brooks <abrooks@live.co.uk>
2020-10-04 01:53:03 +0100

    booleans code complete and tests passed

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const {
 } = require('./lib/strings');
 
 const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
+const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
 
 const app = express();
 
@@ -100,6 +101,26 @@ app.post('/numbers/remainder', (req, res) => {
     res.status(400).json({ error: 'Parameters must be valid numbers.' });
   } else {
     res.status(200).send({ result: remainder(a, b) });
+  }
+});
+app.post('/booleans/negate', (req, res) => {
+  res.status(200).json({ result: negate(req.body.value) });
+});
+app.post('/booleans/truthiness', (req, res) => {
+  res.status(200).json({ result: truthiness(req.body.value) });
+});
+app.get('/booleans/is-odd/:number', (req, res) => {
+  if (isNaN(req.params.number)) {
+    res.status(400).json({ error: 'Parameter must be a number.' });
+  } else {
+    res.status(200).json({ result: isOdd(req.params.number) });
+  }
+});
+app.get('/booleans/:string/starts-with/:char', (req, res) => {
+  if (req.params.char.length > 1) {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  } else {
+    res.status(200).json({ result: startsWith(req.params.char, req.params.string) });
   }
 });
 module.exports = app;
